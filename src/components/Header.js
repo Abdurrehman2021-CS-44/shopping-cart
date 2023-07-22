f68e076cd5ad83ca9c9d5b4b5a1bed011a1063a0
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-22 17:18:01 +0500

    Add hover effect on cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
 const Header = () => {
+
+    const styling = {
+        fontSize: "2rem",
+        color: "white",
+        marginLeft: "0",
+        padding: "0",
+        marginRight: "20px"
+    }
+
     return (
         <>
             <nav className="navbar bg-dark">
                 <div className="container-fluid">
-                    <span className="navbar-brand mb-0 ms-auto h1 text-light">Shop</span>
+                    <span className="navbar-brand mb-0 ms-auto text-light">Shop</span>
+                    <ShoppingCartOutlinedIcon style={styling}/>
                 </div>
             </nav>
         </>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,15 @@
 const Product = (props) => {
     return (
         <>
-            <div className="card text-center" style={{width: "18rem", border: 0}}>
-                <img src={props.image} class="card-img-top" alt="..."/>
+            <div className="card text-center" style={{border: "0"}}>
+                <div className="image-wrapper">
+                    <img src={props.image} alt="..."/>
+                </div>
                 <div className="card-body">
                     <hr />
                     <h5 className="card-title"> <b>{props.title}</b> </h5>
                     <p className="card-text">$ {props.price}</p>
-                    <a href="#" className="btn btn-secondary">Add to Cart</a>
+                    <a href="#" className="btn btn-outline-secondary">Add to Cart</a>
                 </div>
             </div>
         </>
