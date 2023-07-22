92c224fac9e9cfc533ee9293da68414116fb2f9f
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-22 17:28:22 +0500

    Mapping of the products from an external module

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Product from "./Product";
+import products from "../products";
 
 const ProductListing = () => {
     return (
@@ -7,27 +8,19 @@ const ProductListing = () => {
             <Header />
             <div className="container my-5">
                 <div className="row">
-                    <div className="col-lg-4">
-                        <Product 
-                            image="https://i.pinimg.com/236x/13/f4/47/13f447b12d02019d1c5feeeb6165fe80.jpg"
-                            title="Camera"
-                            price="500"
-                        />
-                    </div>
-                    <div className="col-lg-4">
-                        <Product 
-                            image="https://i.pinimg.com/236x/6d/ee/67/6dee67772c774bdf4cc14889b231cb14.jpg"
-                            title="Laptop"
-                            price="600"
-                        />
-                    </div>
-                    <div className="col-lg-4">
-                        <Product 
-                            image="https://i.pinimg.com/236x/0d/6f/28/0d6f285f86d696c90b7ac4122bf5d730.jpg"
-                            title="Ipad"
-                            price="400"
-                        />
-                    </div>
+                    {
+                        products.map((product, index) => {
+                            return (
+                                <div className="col-lg-4 my-4">
+                                    <Product 
+                                        image={product.image}
+                                        title={product.title}
+                                        price={product.price}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </>
