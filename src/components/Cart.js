ab3f6c710241c1ffca6515f298437a5f7f8d6b3b
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-24 01:06:00 +0500

    Implement empty cart functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -34,12 +34,19 @@ const Cart = () => {
                 }
             </div>
             {
-                foundProducts.length > 0 &&
+                foundProducts.length > 0 ?
                 <>
                     <p className="total-price">Total Price: $ {foundProducts.map((product)=>{ return product.price*cart.cartElement[product.id]}).reduce((acc,price)=>{return acc+price})}</p>
                     <div className="btn-positioning">
                         <Link to="/"> <a className="btn btn-dark btn-lg">Continue Shopping</a> </Link>
                         <a className="btn btn-dark btn-lg" onClick={handleClick}>Checkout</a>
+                    </div>
+                </>
+                :
+                <>
+                    <h2 className="empty">Your cart is currently empty. Start shopping now!</h2>
+                    <div className="btn-positioning-empty">
+                        <Link to="/"> <a className="btn btn-dark btn-lg">Continue Shopping</a> </Link>
                     </div>
                 </>
             }
