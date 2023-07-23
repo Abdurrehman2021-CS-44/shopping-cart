b29530950ddfb0ea6473bdbafb73ed4e117a4331
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-23 22:16:31 +0500

    Implement cart and cart element

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,7 @@ import Header from "./Header";
 import CartElement from "./CartElement";
 import ProductContext from "../context/productContext";
 import { useContext } from "react";
+import products from "../products";
 
 const Cart = () => {
     const cart = useContext(ProductContext);
@@ -13,13 +14,15 @@ const Cart = () => {
             <Header />
             <div className="cart-elements">
                 {
-                    cart.cartElement.map((element, index)=>{
+                    products.filter((product, index)=>{
+                        return cart.cartElement[index] > 0 && cart.cartElement.hasOwnProperty(product.id);
+                    }).map((product, index)=>{
                         return (
                             <CartElement
-                                image={element.image}
-                                title={element.title}
-                                price={element.price}
-                                count={element.count}
+                                image={product.image}
+                                title={product.title}
+                                price={product.price}
+                                count={cart.cartElement[index]}
                             />
                         )
                     })
